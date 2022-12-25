1ca74aaf8662b61d8c0c5129280e90d2d997cdb1
Brian Dande <briandande@gmail.com>
2022-12-25 08:37:43 +0300

    Alternative

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 1-element_at.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Author: Jeremy
 
 
 def element_at(my_list, idx):
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # 3-print_reversed_list_integer.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Author: Jeremy
 
 
 def print_reversed_list_integer(my_list=[]):
-    """Print all integers of a list in reverse order."""
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i)))
+    count = len(my_list)-1
+    new_list = []
+    while count >= 0:
+        new_list.append(my_list[count])
+        count -= 1
+    for i in new_list:
+        print("{:d}".format(i))
