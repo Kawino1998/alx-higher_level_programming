ebc7eec4201f6c7c759a76407774f40dac1c6fd0
Brian Dande <briandande@gmail.com>
2023-01-09 17:13:59 +0300

    Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Author: Jeremy
+#Class MyList inheriting from list
+
+class MyList(list):
+    """Class with method - print_sorted"""
+
+    def print_sorted(self):
+        """Method that sorts a list"""
+
+        print(sorted(list(self))
