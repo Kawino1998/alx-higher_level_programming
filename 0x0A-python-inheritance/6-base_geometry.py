4bbcbc24468b861bf33f265de4a492fbc5700d74
Brian Dande <briandande@gmail.com>
2023-01-09 18:23:16 +0300

    Unevaluated area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
+
+
+def area(self):
+    """Raises an exception when area is not evaluated"""
+
+    raise Exception('area() is not implemented')
