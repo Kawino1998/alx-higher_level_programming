bf10af40ee8a64ba8ea003d521094786df90573d
Brian Dande <briandande@gmail.com>
2023-01-09 18:07:31 +0300

    Fixed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,7 @@ module with method is_kind_of_class
 ================================
 """
 
+
 def inherits_from(obj, a_class):
     """Method that return True if an object is an instance of a class
     that inherited from"""
