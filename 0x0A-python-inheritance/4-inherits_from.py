eafa11271c27207d6d3d064e0b1a70ed14c0f4c9
Brian Dande <briandande@gmail.com>
2023-01-09 17:54:33 +0300

    Check

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+================================
+Whether an object is instance of a class
+================================
+"""
+
+def inherits_from(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
+    return False
