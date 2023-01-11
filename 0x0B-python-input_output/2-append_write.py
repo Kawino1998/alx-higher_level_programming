3ea191391c25959b1c211d0f2e958a26145192a7
Brian Dande <briandande@gmail.com>
2023-01-11 19:07:19 +0300

    fixed

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+write_file function module.
+Define write_file function.
+"""
 
 
-def append_write(filename="", text=""):
-    with open(filename, mode="a", encoding="utf-8") as myfile:
-        characters = myfile.write(text)
-    myfile.close()
-    return characters
+def append_file(filename="", text=""):
+    """Appends a string to a text file (UTF8).
+    filename (str): the file, must have permissions, will create if nonexist.
+    text (str): text, will overwrite content of file if already exists.
+    Returns: the number of characters added.
+    """
+    with open(filename, 'a', encoding="UTF-8") as myfile:
+        return myfile.write(text)
