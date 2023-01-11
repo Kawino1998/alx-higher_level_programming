59c56588a747189730cfb5c7f4358ccb151bd291
Brian Dande <briandande@gmail.com>
2023-01-11 13:29:39 +0300

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,9 +8,8 @@ Define read_file function.
 
 
 def read_file(filename=""):
-    with open(filename, encoding='UTF8') as f:
-        """Reads a text file (UTF8) and prints it to stdout.
-           filename (file):
-           """
-        data = f.read()
-        print(data)
+    """Reads a text file (UTF8) and prints it to stdout.
+    filename (file):
+    """
+    data = f.read()
+    print(data)
