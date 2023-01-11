4ab1e85856d322fd31b905d192169c81b6f5561d
Brian Dande <briandande@gmail.com>
2023-01-11 19:32:24 +0300

    fixed

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,6 +5,7 @@ from_json_string function module.
 Define from_json_string function.
 """
 
+
 def from_json_string(my_str):
     """Returns an object made from JSON string.
     my_str: the string to load from JSON.
