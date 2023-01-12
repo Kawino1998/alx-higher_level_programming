6754129f67af76b4b86e2dd21f03c1e3574b45be
Brian Dande <briandande@gmail.com>
2023-01-12 14:01:34 +0300

    fixed

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+#File: 6-load_from_json_file.py
+#Author: Jeremy Kawino
+"""Defines a JSON file-reading function."""
 import json
 
 
 def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
     with open(filename, mode="r") as myfile:
         return json.loads(myfile.read())
