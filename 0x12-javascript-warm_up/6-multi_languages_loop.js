7a9ac1ecc102c29309eb04ba1ae9530de4d12226
Kawino1998 <jeremykawino@gmail.com>
2023-03-13 19:01:43 +0000

    fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myVar = ['C is fun', 'Python is cool', 'JavaScript is amaizing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amaizing'];
 for (i = 0; i < myVar.length; i++){
     console.log(myVar[i]);
 }
