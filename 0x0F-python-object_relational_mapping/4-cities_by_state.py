0f15da7bc66048d20bb1543bf509dea81a13102d
Kawino1998 <jeremykawino@gmail.com>
2023-06-18 14:10:06 +0300

    orm

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
