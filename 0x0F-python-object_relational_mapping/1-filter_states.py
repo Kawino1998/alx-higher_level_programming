983d6a02e6eeb1cfc31a99cc025d123f0b579195
Kawino1998 <jeremykawino@gmail.com>
2023-06-18 10:36:31 +0300

    ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3])
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM persons WHERE LastName LIKE "N%"')
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
