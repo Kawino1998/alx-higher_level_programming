7173f8f89fd60424783cc07d4266b280d041e3d6
Kawino1998 <jeremykawino@gmail.com>
2023-06-18 12:28:35 +0300

    orm

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE BINARY `name` = '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
+    cur.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
