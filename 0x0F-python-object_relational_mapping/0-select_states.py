d7a8fd054fb477f238b612c29ea07ea5ed0a9222
Kawino1998 <jeremykawino@gmail.com>
2023-06-18 09:03:27 +0300

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,18 @@ the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(host="localhost", port="3306", 
-        user=sys.argv[1], 
-        passwd=sys.argv[2], db=sys.argv[3])
-c = conn.cursor()
-c.execute('SELECT * FROM persons')
-rows = c.fetchall()
-for row in rows:
-    print(row)
+if __name__ == '__main__':
+
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    conn = MySQLdb.connect(host="localhost", port="3306", 
+            user=sys.argv[1], 
+            passwd=sys.argv[2], db=sys.argv[3])
+    c = conn.cursor()
+    c.execute('SELECT * FROM persons')
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
 
