32f0be8c2b3718625b04295dabdff1b02832e3fe
Kawino1998 <jeremykawino@gmail.com>
2023-06-19 22:27:02 +0300

    ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     from the database.
     """
     conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                            passwd=argv[2], db=argv[3])
+                           passwd=argv[2], db=argv[3])
 
     c = conn.cursor()
     c.execute("SELECT * FROM states \
